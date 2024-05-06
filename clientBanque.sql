14ebae623b1e8c0e78f456a676bc6504079305b1
HeriniavoFaly <heriniavofaly@gmail.com>
2024-05-06 23:49:08 +0100

    update database, update volume sql

--- clientBanque.sql
+++ clientBanque.sql
@@ -2,14 +2,14 @@ create database IF NOT exists clientBanque;
 USE clientBanque;
 
 create table IF NOT exists client(
-idClient int primary key not null,
+idClient int primary key not null AUTO_INCREMENT,
 nom varchar(50) not null,
 prenom varchar(50) not null,
 solde float not null
 );
 
-insert into client values
-(1, "Heriniavo", "Faly", 12000),
-(2, "Chioua", "Hiba", 10000),
-(3, "El bejaoui", "Nada", 10000),
-(4, "Aya", "Aya", 10000);
+-- insert into client values
+-- (1, "Heriniavo", "Faly", 12000),
+-- (2, "Chioua", "Hiba", 10000),
+-- (3, "El bejaoui", "Nada", 10000),
+-- (4, "Aya", "Aya", 10000);
